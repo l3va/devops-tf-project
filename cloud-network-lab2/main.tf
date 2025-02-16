3fa8265e96111deacabaf86fc120de7855cba219
l3va <ggwpleva@gmail.com>
2025-02-16 14:42:51 +0000

    added networking resource into main configuration

---
+++ cloud-network-lab2/main.tf
@@ -0,0 +1,48 @@
+locals {
+  common_tags = {
+    ManagedBy = "Terraform"
+    Project   = "cloud-network-lab2"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = merge(local.common_tags, {
+    Name = "vnet-nebo"
+  })
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/17"
+
+  tags = merge(local.common_tags, {
+    Name = "snet-public"
+  })
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "snet-igw"
+  })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "snet-rt"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
